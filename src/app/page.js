e6f01885f793e5b5b45ef5c085231db2c4e0247f
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-05-23 10:57:54 +0700

    coba

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex">
-        <p className="text-white text-6xl">COBA COBA COBA COBA</p>
+        <p className="text-white text-6xl">COBA COBA COBA COBA BROUUUUU</p>
       </div>
     </main>
   );
