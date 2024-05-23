add7f9b8e4f0eac883eb9b6cce96c517aa620893
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2024-05-23 09:40:31 +0700

    add babel

---
+++ dist/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+export default function Home() {
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex">
+        <p className="text-white text-6xl">COBA COBA COBA COBA</p>
+      </div>
+    </main>;
+}
